4261231cb314b6048c2ac9037d07bb2e05578e04
Ewan-Dev <ewanmccairn2010@gmail.com>
2025-12-06 21:43:47 +0000

    feat(Tensor): add gradient tracking - just added if we want to track or not

--- tensors.py
+++ tensors.py
@@ -1,26 +1,32 @@
 class Tensor:
     # When initalised...
-    def __init__(self,data):
+    def __init__(self,data, requires_grad=False):
         self.data = data
         self.grad = 0
+        self.require_grad = requires_grad
 
     # Return on multiplication...
     def __mul__(self, other):
-        return Tensor(self.data * other)
+        other_data = other.data if isinstance(other, Tensor) else other
+        return Tensor(self.data * other_data, requires_grad=self.require_grad)
     
     # Right-hand-side multiplication
     def __rmul__(self, other):
-        return (self.data * other)
+        other_data = other.data if isinstance(other, Tensor) else other
+        return Tensor(self.data * other_data, requires_grad=self.require_grad)
     
     # Left-hand and right-hand subtraction
     def __sub__(self, other):
-        return (self.data - other)
-
+        other_data = other.data if isinstance(other, Tensor) else other
+        return Tensor(self.data - other_data, requires_grad=self.require_grad)
     def __rsub__(self, other):
-        return (other - self.data)
+        other_data = other.data if isinstance(other, Tensor) else other
+        return Tensor(other_data - self.data, requires_grad=self.require_grad)
     
   # Left-hand and right-hand power
     def __pow__(self, other):
-        return (self.data ** other)
+        other_data = other.data if isinstance(other, Tensor) else other
+        return Tensor(self.data ** other_data, requires_grad=self.require_grad)
     def __rpow__(self, other):
-        return (other ** self.data )
+        other_data = other.data if isinstance(other, Tensor) else other
+        return Tensor(other_data ** self.data, requires_grad=self.require_grad)
